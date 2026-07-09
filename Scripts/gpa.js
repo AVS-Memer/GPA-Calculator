02e7da8ff717eef4f6e959764ab4d679dadd7eed
AVS <123315354+AVS-Memer@users.noreply.github.com>
2026-07-09 10:31:18 -0700

    Update gpa.js

--- Scripts/gpa.js
+++ Scripts/gpa.js
@@ -1,32 +1,45 @@
 let courses = JSON.parse(localStorage.getItem("myCourses")) || [];
 
-let result = calculateAllCourseGPA(courses);
+// All course GPA
+let allCourseResult = calculateAllCourseGPA(courses);
 
-document.getElementById("gpa").innerHTML = result.GPA.toFixed(2);
+document.getElementById("allCourseGPA").innerHTML =
+allCourseResult.GPA.toFixed(2);
+document.getElementById("allCourseCredits").innerHTML =
+"Credits: " + allCourseResult.Credits;
 
-document.getElementById("credits").innerHTML = "Credits counted: " + result.Credits;
+// Core GPA
+let coreResult = calculateAllCourseGPA(courses);
+
+document.getElementById("coreGPA").innerHTML = "--";
+
+document.getElementById("coreCredits").innerHTML = "Credits: --";
 
 let box = document.getElementById("courses");
 
-if (result.Courses.length === 0) box.innerHTML = "No graded courses found.";
-
+if (coreResult.Courses.length === 0) box.innerHTML = "Core GPA course breakdown will appear here.";
 else {
-  result.Courses.forEach(course=>{
-    let div = document.createElement("div");
-    div.className="course";
-    div.innerHTML =
-    `
-    <b>${course.Course_Name}</b><br>
-    ${course.Level}
-    |
-    ${course.Grade}
-    <br>
-    GPA Value:
-    ${course.GPA_Points.toFixed(2)}
-    <br>
-    Credits:
-    ${course.Credits}
-    `;
-    box.appendChild(div);
-  });
+    result.Courses.forEach(course=>{
+        let div =
+        document.createElement("div");
+        div.className="course";
+        div.innerHTML = `
+        <b>${course.Course_Name}</b><br>
+
+        ${course.Level}
+        |
+        ${course.Grade}
+
+        <br>
+
+        GPA Value:
+        ${course.GPA_Points.toFixed(2)}
+
+        <br>
+
+        Credits:
+        ${course.Credits}
+        `;
+        box.appendChild(div);
+    });
 }
