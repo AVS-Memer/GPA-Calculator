f84af715f1c04e84cfff1eba368844aa703c6473
AVS <123315354+AVS-Memer@users.noreply.github.com>
2026-07-11 09:06:31 -0700

    Update navbar.js

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
   if (!navbar) return;
   navbar.innerHTML = `
     <nav class="navbar">
-      <div class="logo"><a href="index.html">GPA Tracker</a></div>
+      <div class="logo"><a href="index.html">GPA Calculator</a></div>
       <div class="nav-links">
-        <a href="index.html">Home</a>
-        <a href="mySchedule.html">My Schedule</a>
-        <a href="whatIf.html">What If</a>
-        <a href="catalog.html">Catalog</a>
-        <a href="rank.html">Rank Estimator</a>
-        <a href="settings.html">Settings</a>
+        <a href="">Home</a>
+        <a href="catalog">Catalog</a>
+        <a href="mySchedule">My Schedule</a>
+        <a href="gpa">GPA Calculator</a>
+        <a href="whatIf">What If</a>
+        <a href="rank">Rank Estimator</a>
       </div>
     </nav>
   `;
